0b45ab1a07727170d997d30b34e0a91271d51540
leordefreitas <leonardodefreitas1995@yahoo.com.br>
2020-06-04 12:49:42 -0300

    create function firebase, of cretae test

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -1,8 +1,40 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// to get information for firebase
+exports.getTest = functions.https.onRequest((req, res) => {
+  admin.firestore().collection('test').get()
+  .then((doc) => {
+    let testArray = [];
+    doc.forEach((docToPush) => {
+      testArray.push(docToPush.data());
+    });
+    return res.json(testArray);
+  })
+  .catch((err) => console.error(err));
+});
+
+// to create data
+exports.createTest = functions.https.onRequest((req, res) => {
+
+// creating
+  const newTest = {
+    name: req.body.name,
+    body: req.body.body,
+    date: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+// posting
+  admin.firestore().collection('test').add(newTest)
+  .then((doc) => {
+    res.json({message: `ID(${doc.id}) created with sucess`});
+  })
+  .catch((err) => {
+    res.status(500).json({error: 'problem with the creation'});
+    console.error(err);
+  });
+});
