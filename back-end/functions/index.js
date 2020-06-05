1a9716f9a180f150f2ce3ee747631afb85882140
leordefreitas <leonardodefreitas1995@yahoo.com.br>
2020-06-05 08:38:19 -0300

    developing the creation of user function

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -5,6 +5,23 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// firebase library
+const firebase = require('firebase');
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBuGAluHUpfjwbU1iZmrC2GEAt6KWJpqBY",
+  authDomain: "malumodas-aaaeb.firebaseapp.com",
+  databaseURL: "https://malumodas-aaaeb.firebaseio.com",
+  projectId: "malumodas-aaaeb",
+  storageBucket: "malumodas-aaaeb.appspot.com",
+  messagingSenderId: "836282117124",
+  appId: "1:836282117124:web:3c94325883ac6910995951",
+  measurementId: "G-D2X2V98ZP0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 // express library
 const express = require('express');
 const app = express();
@@ -29,20 +46,17 @@ app.get('/test', (req, res) => {
 
 // to create data
 app.post('/test', (req, res) => {
-
   // method to just post
   if(req.method !== 'POST') {
     res.status(400).json({error: 'only can be (POST) method'})
   };
-
-// creating
+  // creating
   const newTest = {
     name: req.body.name,
     body: req.body.body,
     date: new Date().toISOString()
   };
-
-// posting
+  // posting
   admin.firestore().collection('test').add(newTest)
   .then((doc) => {
     res.json({message: `ID(${doc.id}) created with sucess`});
@@ -50,6 +64,29 @@ app.post('/test', (req, res) => {
   .catch((err) => {
     res.status(500).json({error: 'problem with the creation'});
     console.error(err);
+  });
+});
+
+// to create new user
+// creating
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    name: req.body.name,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    whatsapp: req.body.whatsapp
+  };
+  // validating information
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  .then(doc => {
+    return res.status(201).json({message: `ID ${doc.user.uid} create with sucess!`})
+  })
+  .catch(err => {
+    console.error(err);
+    return res.status(500).json({
+      error: `${err.code} something went wrong with the creation of the user`
+    });
   });
 });
 
