5164fc0430ed75a7e2c7f9a22be80c6e6ce99c0d
leordefreitas <leonardodefreitas1995@yahoo.com.br>
2020-06-10 08:33:36 -0300

    fixing bugs functions firebase

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -22,42 +22,46 @@ const app = express();
 const db = admin.firestore();
 
 // TO GET THE DATA
-app.get('/test', (req, res) => {
-  db.collection('test').orderBy('date', 'desc').get()
+app.get('/products', (req, res) => {
+  db.collection('products').orderBy('date', 'desc').get()
   .then((doc) => {
-    let testArray = [];
+    let productsArray = [];
     doc.forEach((docToPush) => {
-      testArray.push({
+      productsArray.push({
         id: docToPush.id,
-        name: docToPush.data().name,
-        body: docToPush.data().body,
-        date: docToPush.data().date
+        type: docToPush.data().type,
+        material: docToPush.data().material,
+        color: docToPush.data().color,
+        size: docToPush.data().size,
+        createAt: docToPush.data().date
       });
     });
-    return res.json(testArray);
+    return res.json(productsArray);
   })
   .catch((err) => console.error(err));
 });
 
 // TO CREATE NEW DATA
-app.post('/test', (req, res) => {
+app.post('/products', (req, res) => {
   // method to just post
   if(req.method !== 'POST') {
-    res.status(400).json({error: 'only can be (POST) method'})
+    res.status(400).json({error: 'Only can be POST method'})
   };
   // creating
-  const newTest = {
-    name: req.body.name,
-    body: req.body.body,
-    date: new Date().toISOString()
+  const newProduct = {
+    type: req.body.type,
+    material: req.body.material,
+    color: req.body.color,
+    size: req.body.size,
+    createAt: new Date().toISOString()
   };
   // posting
-  db.collection('test').add(newTest)
+  db.collection('products').add(newProduct)
   .then((doc) => {
     res.json({message: `ID(${doc.id}) created with sucess`});
   })
   .catch((err) => {
-    res.status(500).json({error: 'problem with the creation'});
+    res.status(500).json({error: 'Problem with the creation'});
     console.error(err);
   });
 });
@@ -65,16 +69,16 @@ app.post('/test', (req, res) => {
 // TO CREATE NEW USER
 // functions to validate some informations
 const emptyString = (string) => {
-  if (string.trim() === '') true;
-  else false;
+  if (string.trim() === '') return true;
+  else return false;
 };
 const isEmail = (email) => {
   const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.match(emailRegEx)) true;
-  else false;
+  if (email.match(emailRegEx)) return true;
+  else return false;
 };
 const isWhatsapp = (whatsappNumber) => {
-  if (typeof(whatsappNumber) === 'number' && whatsappNumber.toString().length === 11) return true;
+  if (whatsappNumber.toString().length === 11) return true;
   else return false;
 };
 
@@ -97,23 +101,32 @@ app.post('/signup', (req, res) => {
   } else if (!isEmail(newUser.email)) {
     errorsOfValidations.email = 'This is not a email adress'
   }
-
+  // password
   if (emptyString(newUser.password)) {
     errorsOfValidations.password = 'Password must be not empty'
   } else if (newUser.password !== newUser.confirmPassword) {
     errorsOfValidations.password = 'Password must be equal to confirm password'
   }
+  // whatsapp
+  if (emptyString(newUser.whatsapp)) {
+    errorsOfValidations.whatsapp = 'Whatsapp must be not empty'
+  } else if (!isWhatsapp(newUser.whatsapp)) {
+    errorsOfValidations.whatsapp = 'Whatsapp need 11 numbers'
+  }
+  // name
+  if (emptyString(newUser.name)) {
+    errorsOfValidations.name = 'Name must be not empty'
+  }
+  // errorsOfValidations
+  if (Object.keys(errorsOfValidations).length > 0) res.status(400).json(errorsOfValidations);
 
-
-
-
-  // validating information
+// validating information
   let token = null;
   let userId = null;
   db.doc(`/users/${newUser.email}`).get()
   .then(doc => {
     if (doc.exists) {
-      return res.status(400).json({email: `this email already exists`});
+      return res.status(400).json({email: `This email already exists`});
     } else {
       return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
     }
@@ -134,16 +147,43 @@ app.post('/signup', (req, res) => {
     };
     return db.doc(`/users/${newUser.email}`).set(userCredentials);
   })
-  .then((nothing) => {
+  .then(() => {
     return res.status(201).json({ token });
   })
   .catch(err => {
     console.error(err);
     return res.status(500).json({
-      error: `something went wrong with your new user ${err.code}`
+      error: `Something went wrong with your new user, ${err.code}`
     });
   });
 });
+
+// TO LOGING
+app.post('/login', (req, res) => {
+  const userToLogin = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  // validating errors
+  let errorsOfLogin = {};
+  if (emptyString(userToLogin.email)) return errorsOfLogin.email = 'Must not be empty';
+  if (emptyString(userToLogin.password)) return errorsOfLogin.password = 'Must not be empty';
+  if(Object.keys(errorsOfLogin).length > 0) return res.status(400).json(errorsOfLogin);
+
+  // actually logining
+  firebase.auth().signInWithEmailAndPassword(userToLogin.email, userToLogin.password)
+  .then(data => {
+    return data.user.getIdToken();
+  })
+  .then(token => {
+    return res.json({token})
+  })
+  .catch(err => {
+    return res.status(500).json({err})
+  })
+});
+
 
 // EXPORTING BY EXPRESS
 exports.api = functions.https.onRequest(app);
