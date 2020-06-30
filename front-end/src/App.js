b5a71b09c22ba4a1da76f2cf75e93c7a50bd233e
leordefreitas <leonardodefreitas1995@yahoo.com.br>
2020-06-30 11:19:42 -0300

    implementing color, some martial-ui things in the navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
+// LIBRARYS
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// PAGES
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+// COMPONENTS
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+// material-ui
+// `this is here becouse all pages will have
+// `the same theme, all this is in the website
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar/>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home}/>
+                <Route exact path="/login" component={login}/>
+                <Route exact path="/signup" component={signup}/>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
