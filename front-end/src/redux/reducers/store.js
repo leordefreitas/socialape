d3b6524d236ac9a66444e05d870901adeced24d2
leordefreitas <leonardodefreitas1995@yahoo.com.br>
2020-07-01 12:22:47 -0300

    creating new files of the redux configuration

---
+++ front-end/src/redux/reducers/store.js
@@ -0,0 +1,42 @@
+// createStore is to create a new storage
+// combineReducers is to get together more then one reduce
+// applyMidleware is to get a security to the aplications
+// compose ??
+import { 
+  createStore, 
+  combineReducers, 
+  applyMiddleware, 
+  compose 
+} from 'redux';
+import thunk from 'redux-thunk';
+
+// IMPORTING THE REDUCERS
+import userReducers from './userReducers';
+import uiReducers from './uiReducers';
+import dataReducers from './dataReducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+// this is the reducers to became one 
+const reducers = combineReducers({
+  user: userReducers,
+  UI: uiReducers,
+  data: dataReducers
+});
+
+// store
+// `here we create this is all in the website redux
+// `the code window was take from tyhe site becouse allow us
+// `to see in the devtools better what file is goin where
+const store = createStore(
+  reducers, 
+  initialState, 
+  compose(applyMiddleware(
+    ...middleware, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  )
+);
+
+export default store;
